b0593aacaed9e190d2a4ffc6b3da11cc218375e0
kritika <singhalkritika0708@gmail.com>
2024-05-28 16:52:20 +0530

    Deque

---
+++ src/Queue/DequeExample.java
@@ -0,0 +1,13 @@
+package Queue;
+import java.util.*;
+
+public class DequeExample {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        System.out.println(deque);
+        deque.removeLast();
+        System.out.println(deque);
+    }
+}
