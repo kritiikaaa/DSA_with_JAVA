8b7a5cd93ba0cdea331d7729b6e48e6243310b8e
kritika <singhalkritika0708@gmail.com>
2024-05-29 10:54:20 +0530

    Queue using Deque

---
+++ src/Queue/QueueUsingDeque.java
@@ -0,0 +1,28 @@
+package Queue;
+import java.util.*;
+
+public class QueueUsingDeque {
+    static class Queue{
+        Deque<Integer> deque = new LinkedList<>();
+        //add
+        public void add(int data){
+            deque.addFirst(data);
+        }
+        //remove
+        public int remove(){
+            return deque.removeFirst();
+        }
+        //peek
+        public int peek(){
+            return deque.getFirst();
+        }
+    }
+    public static void main(String[] args) {
+       Queue q = new Queue();
+       q.add(1);
+       q.add(2);
+       q.add(3);
+       System.out.println(q.peek());
+
+    }
+}
