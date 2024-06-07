bec5d680644f117a8c672db7ffe29b6b1d086d08
kritika <singhalkritika0708@gmail.com>
2024-06-07 14:44:54 +0530

    Traversal

---
+++ src/BinaryTree/Postorder.java
@@ -0,0 +1,48 @@
+package BinaryTree;
+
+public class Postorder {
+        static class Node{
+            int data;
+            Node left;
+            Node right;
+
+            Node(int data){
+                this.data= data;
+                this.left = null;
+                this.right = null;
+            }
+        }
+
+        static class BinaryTree{
+            static int idx = -1;
+            public static Node buildTree(int[] nodes){
+                idx++;
+                if(nodes[idx] == -1){
+                    return null;
+                }
+
+                Node NewNode = new Node(nodes[idx]);
+                NewNode.left = buildTree(nodes);
+                NewNode.right = buildTree(nodes);
+                return NewNode;
+            }
+
+            public static void postOrder(Node root){
+                if(root==null) {
+                    return;
+                }
+                postOrder(root.left);
+                postOrder(root.right);
+                System.out.print(root.data+" ");
+            }
+        }
+
+        public static void main(String[] args) {
+            int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+            BinaryTree tree = new BinaryTree();
+            Node root = tree.buildTree(nodes);
+            tree.postOrder(root);
+        }
+    }
+
+
