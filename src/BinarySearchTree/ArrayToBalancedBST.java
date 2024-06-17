ba72f7899a976ec02d6b8164f462b50fb3adcf33
kritika <singhalkritika0708@gmail.com>
2024-06-17 16:36:58 +0530

    Create Balanced BST using sorted Array

---
+++ src/BinarySearchTree/ArrayToBalancedBST.java
@@ -0,0 +1,39 @@
+package BinarySearchTree;
+
+public class ArrayToBalancedBST {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){this.data = data;
+        }
+    }
+
+    public static Node balancedBST(int[] arr, int start, int end){
+        if(start>end){
+            return null;
+        }
+            int mid = (start+end)/2;
+            Node root = new Node(arr[mid]);
+            root.left = balancedBST(arr,start,mid-1);
+            root.right = balancedBST(arr, mid+1, end);
+        return root;
+    }
+
+    public static Node preOrder(Node root){
+        if(root==null){
+            return null;
+        }
+        System.out.print(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,5,6,8,10,11,12};
+        Node root = balancedBST(arr,0,arr.length-1);
+        preOrder(root);
+    }
+}
