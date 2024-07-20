41cc7b92654d93cbb171c4b951d10ba51406ffee
kritika <singhalkritika0708@gmail.com>
2024-07-20 23:00:25 +0530

    Trie creation

---
+++ src/Tries/Creation.java
@@ -0,0 +1,21 @@
+package Tries;
+
+public class Creation {
+
+    static class Node{
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node(){
+           for(int i=0; i<26; i++){
+               children[i] = null;
+           }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void main(String[] args) {
+        String word[] = {"the", "a", "there", "their", "any", "thee"};
+    }
+}
