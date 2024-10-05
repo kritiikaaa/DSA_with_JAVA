504fd2d63334e01df79c24c962b7dc0d84c31a52
kritika <singhalkritika0708@gmail.com>
2024-10-05 14:29:50 +0530

    Print nth fibonacci number

---
+++ src/Recursion/fibonacci.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class fibonacci {
+    public static int fibo (int n){
+        if(n==0 || n==1){
+            return n;
+        }
+        int fn = fibo(n-1) + fibo(n-2);
+        return fn;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(fibo(n));
+    }
+}
